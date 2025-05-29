8abc2ef279c7efaa466c957c47509347480a4740
AdityaTechPrime <an2924283@gmail.com>
2025-05-29 19:04:32 +0530

    updated media query

--- MediaQueryController.js
+++ MediaQueryController.js
@@ -7,7 +7,10 @@
 export class MediaQueryController {
     constructor() {
         this.mediaQuery = window.matchMedia('(max-width: 600px)');
+        this.mediaQuery500 = window.matchMedia('(max-width: 500px)');
         this.speedHeading = document.querySelector('.speed-heading-text'); // Targeted by new class in HTML
+        this.contact = document.querySelector('.txtContact'); // Targeted by new class in HTML
+
         this.rangeContainer = document.querySelector('.range-container');
 
         if (!this.speedHeading || !this.rangeContainer) {
@@ -39,10 +42,14 @@ export class MediaQueryController {
             // Screen is 600px or less
             this.speedHeading.textContent = "Speed W.P.M.";
             this.rangeContainer.classList.add('range-container-small');
+
+            this.contact.textContent = " ";
         } else {
             // Screen is wider than 600px
             this.speedHeading.textContent = "Select your speed in Words Per Minute (WPM)";
             this.rangeContainer.classList.remove('range-container-small');
+
+            this.contact.textContent = "Contact";
         }
     }
 }
